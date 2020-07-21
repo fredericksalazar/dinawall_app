75739b985e82bacc2c2fbbfddf2f686122b6ab5e
Frederick Salazar Sanchez <fredefass01@gmail.com>
2020-07-20 21:31:36 -0500

    The logic has been implemented to control the single execution of intance, when the daemon is started by the operating system and the app is run again, it will open the ui of the running instance

--- src/main/java/dinawall_app/DinawallApp.java
+++ src/main/java/dinawall_app/DinawallApp.java
@@ -8,7 +8,6 @@ package dinawall_app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,11 +15,19 @@ import dinawall_app.controller.PrimarySceneController;
 import dinawall_app.model.DinaWallAppModel;
 import dinawall_app.ui.DinaWallpaperComponent;
 import dinawall_core.wallpaper.DinaWallpaper;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javafx.application.Platform;
+import javafx.scene.layout.BorderPane;
 
 public class DinawallApp extends Application{
     
     private Stage primaryStage;
-    private HBox rootLayout;
+    private BorderPane rootLayout;
     private FXMLLoader loader;
     
     private PrimarySceneController controller;
@@ -29,19 +36,22 @@ public class DinawallApp extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         
+        Platform.setImplicitExit(false);
+        
         dinawall_model = DinaWallAppModel.getInstance();
         
-        this.primaryStage = stage;
-        this.primaryStage.setTitle("DinaWall");
+        primaryStage = stage;
+        primaryStage.setTitle("DinaWall");
                 
         init_primary_scene();
+        startListenerInstance();
         loadDinaWallpapers();
     }
 
     private void init_primary_scene() {
         try{
             loader = new FXMLLoader();
-            loader.setLocation(DinawallApp.class.getResource("/dinawall_app/ui/primary_scene.fxml"));
+            loader.setLocation(DinawallApp.class.getResource("/dinawall_app/ui/dinaWall_app.fxml"));
             
             rootLayout = loader.load();
             
@@ -50,14 +60,19 @@ public class DinawallApp extends Application{
             
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
-                        
+            primaryStage.setFullScreen(false);
+            primaryStage.setMaximized(false);
+            primaryStage.setResizable(false);
+            primaryStage.setOnCloseRequest(eh->{
+                this.primaryStage.hide();
+            });
+            
             primaryStage.show();
             
         }catch(IOException e){
             System.err.println("error init_prmary_scene -> "+e);
         }
     }
-    
     
     /**
      * This method get all dinawallpaper installed and create a dinawallpaper_component
@@ -82,6 +97,77 @@ public class DinawallApp extends Application{
     
     public Stage getMainStage(){
         return this.primaryStage;
+    }
+    
+    
+    /**
+     * This method is used to have a single instance of the app, when
+     * dinawall_daemon has been started as service daemon and the user 
+     * open a new instance of dinawall_app then the system validate and 
+     * invoke a server to open UI.
+     */
+    
+    private void startListenerInstance(){
+        Thread instanceListener = new Thread(()->{
+            
+        try {
+            System.out.println("creating listener instance  ...");
+            
+            ServerSocket server = new ServerSocket(35531);
+            
+            try{
+                
+                while(true){
+                    Socket client = server.accept();
+
+                    BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    String request = input.readLine();
+
+                    System.err.println("Request from client -> "+request);
+                    
+                    if(request.equals("start app")){
+                    
+                    System.err.println("show the dinawall_app from server ...");
+
+                    Platform.runLater(() ->{
+                        this.primaryStage.show();
+                        this.primaryStage.toFront();
+                        System.out.println("Launch the new ui instance ...");
+                    });
+                }
+                }
+                
+                
+            }catch(IOException io){
+                System.out.println("dinawall_app.controller.DinaWallServer.run()");
+            }
+        } catch(BindException e){
+            try {
+                System.err.println("Other instance is runing in this moment, then open the app ui ...");
+                
+                /**
+                 * create a socket client to send message to other instance that
+                 * show the app ui
+                 */ 
+                
+                try (Socket clientSocket = new Socket("127.0.0.1", 35531); 
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream())) {
+                     out.println("start app");
+                    }
+            } catch (IOException ex) {
+                System.out.println("dinawall_app.DinawallApp.startListenerInstance()");
+            }
+            
+            System.exit(0);
+            
+        } catch (IOException ex) {
+            System.out.println("dinawall_app.DinawallApp.startListenerInstance()");
+        }
+        
+        },"dinawall_server_instance");
+        
+        instanceListener.setDaemon(true);
+        instanceListener.start();
     }
         
 }
